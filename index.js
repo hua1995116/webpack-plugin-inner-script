f95e476962753b5f1e6de2e1a778d4b49db23cc2
hua1995116 <461249104@qq.com>
2018-05-07 10:49:08 +0800

    U: judge js

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ function WebpackPluginInnerScript(options) {
 WebpackPluginInnerScript.prototype.apply = function (compiler) {
     var self = this;
     if (compiler.hooks) {
-        compiler.hooks.compilation.tap('HtmlWebpackReplace',
-            function (compilation) {
-                compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync('HtmlWebpackReplace', self.innerScript.bind(self));
-            });
+        compiler.hooks.compilation.tap('HtmlWebpackReplace', function (compilation) {
+            compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync(
+                'HtmlWebpackReplace',
+                self.innerScript.bind(self)
+            );
+        });
     } else {
         compiler.plugin('compilation', compilation => {
-            compilation.plugin('html-webpack-plugin-alter-asset-tags', self.innerScript.bind(self));
+            compilation.plugin(
+                'html-webpack-plugin-alter-asset-tags',
+                self.innerScript.bind(self)
+            );
         });
     }
 };
 
-WebpackPluginInnerScript.prototype.innerScript = function (htmlPluginData, callback) {
+WebpackPluginInnerScript.prototype.innerScript = function (
+    htmlPluginData,
+    callback
+) {
     if (this.options.hasOwnProperty('innerScript') && this.options.innerScript) {
         ['head', 'body'].map(position => {
             for (var i = 0; i < htmlPluginData[position].length; i++) {
-                let isIgnore = false; // js
+                let isIgnore = false;
                 const url = htmlPluginData[position][i].attributes.src;
                 const template = htmlPluginData.outputName;
-                if (this.options.hasOwnProperty('ignore') && this.options.ignore.length > 0) {
+                if (
+                    this.options.hasOwnProperty('ignore') &&
+                    this.options.ignore.length > 0
+                ) {
                     this.options.ignore.map(item => {
                         if (istype(item, 'RegExp') && item.test(url)) {
                             isIgnore = true;
@@ -34,24 +45,25 @@ WebpackPluginInnerScript.prototype.innerScript = function (htmlPluginData, callb
                     break;
                 }
 
-                let isInclude = false; // html
-                const hasInclude = this.options.hasOwnProperty('include');
-                if (hasInclude && this.options.include.length > 0) {
+                let isInclude = false;
+                if (
+                    this.options.hasOwnProperty('include') &&
+                    this.options.include.length > 0
+                ) {
                     this.options.include.map(item => {
                         if (istype(item, 'RegExp') && template.match(item)) {
                             isInclude = true;
                         }
                     });
-                } else if(!hasInclude || this.options.include.length === 0) {
-                    isInclude = true;
                 }
-                if (isInclude) {
+                if (isInclude && htmlPluginData[position][i].attributes.type === 'text/javascript') {
                     htmlPluginData[position][i].innerHTML = `
-                    var oHead = document.querySelector('body'),
+                var oHead = document.querySelector('body'),
                     oScript = document.createElement('script');
-                    oScript.type = "text/javascript";
-                    oScript.src = "${url}";
-                    oHead.appendChild(oScript);`;
+                oScript.type = "text/javascript";
+                oScript.src = "${url}";
+                oHead.appendChild(oScript);
+                `;
                     htmlPluginData[position][i].attributes = {
                         type: 'text/javascript'
                     };
@@ -63,7 +75,9 @@ WebpackPluginInnerScript.prototype.innerScript = function (htmlPluginData, callb
 };
 
 function istype(o, type) {
-    return (Object.prototype.toString.call(o) === '[object ' + (type || 'Object') + ']');
+    return (
+        Object.prototype.toString.call(o) === '[object ' + (type || 'Object') + ']'
+    );
 }
 
 module.exports = WebpackPluginInnerScript;
